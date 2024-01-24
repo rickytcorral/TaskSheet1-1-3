5f93391089b67604a4c135097f599070fd79a2ab
rickytcorral <154580337+rickytcorral@users.noreply.github.com>
2024-01-24 23:59:36 +0000

    done

--- OddEven.java
+++ OddEven.java
@@ -1,1 +1,18 @@
+public class OddEven{
+public static void main(String[] args)
+{
+    for(int i=0;i<=10;i++)
+    {
+    if(i%2==0)
+    {
+    System.out.println(i+" - Even number ");
+    }
+    else
+    {
+    System.out.println(i+" - Odd number");
+    }
+    }
+    }
+    }
+
 
